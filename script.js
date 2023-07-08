e3db56c34a6559190e92c21b3805c7ffd388704c
Maha Abdelnabi <mahaomar@gmail.com>
2023-07-07 23:20:42 -0400

    create elements and add it to li

--- script.js
+++ script.js
@@ -4,6 +4,24 @@ const recipeListEl = document.getElementById("recipe-list")
 //function to pass the recipe data as an input
 function displayRecipes(recipes){
     recipeListEl.innerHTML = " "
+    //we want 10 recipes
+    recipes.forEach((recipe) =>{
+     //create elements and add it to the class
+        const recipeItemEl = document.createElement("li");
+        recipeItemEl.classList.add("recipe-item");
+        //add img
+        recipeImgEl = document.createElement("img");
+        recipeImgEl.src = recipe.image;
+        recipeImgEl.alt = "recipe image";
+
+        recipeItemEl.appendChild(recipeImgEl);
+
+    
+
+    //this elements that created..need to add to the recipeListEl
+    recipeListEl.appendChild(recipeItemEl)
+
+    })
 
 }
 
