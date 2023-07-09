400a3bb8e76dd3a57b75208933b2f6c3e5289360
Maha Abdelnabi <mahaomar@gmail.com>
2023-07-08 23:13:36 -0400

    fetch the apt url

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
+const searchBtn = document.getElementById("search-btn");
 
 // event listeners
-searchBtn.addEventListener('click', getMealList);
+searchBtn.addEventListener("click", getMealList);
 
 // get meal list that matches with the ingredients
-function getMealList(){
-    let searchInputTxt = document.getElementById('search-input').value.trim();
-    console.log (searchInputTxt);
+function getMealList() {
+  let searchInputTxt = document.getElementById("search-input").value.trim();
+  fetch(
+    `https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log("Data:", data);
+    });
 }
